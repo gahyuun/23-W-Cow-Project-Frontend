0b44e88175b9eb758d1595fa8cdfa398d190935b
89882 <pgh123166@naver.com>
2023-02-06 00:50:47 +0900

    Style: 변수 네이밍 수정

--- src/pages/Join.jsx
+++ src/pages/Join.jsx
@@ -59,72 +59,72 @@ function Join() {
     alignItems: 'flex-end',
   }; // input 그룹과 formhelpertext를 그룹으로 묶는 스타일
 
-  const options = ['javascript', 'java', 'express'];
+  const options = ['javascript', 'java', 'express']; // select에 들어갈 내용 예시
   const [nickname, setNickname] = React.useState('');
   const [showPW, setShowPW] = React.useState(false); // 비밀번호 보여주기 여부
-  const [isSame, setIsSame] = React.useState(false); // 중복버튼 클릭 여부
+  const [isNameSame, setIsNameSame] = React.useState(false); // 중복버튼 클릭 여부
   const [nicknameError, setNicknameError] = React.useState(''); // 닉네임 에러 text
   const [emailError, setEmailError] = React.useState(''); // 이메일 에러 text
   const [passwordError, setPasswordError] = React.useState(''); // 비밀번호 에러 text
   const [stackError, setStackError] = React.useState(''); // 스택 에러 text
 
-  const handlenicknameChange = (e) => {
+  const handleNickNameChange = (e) => {
     setNickname(e.target.value);
-    setIsSame(false);
+    setIsNameSame(false);
     setNicknameError('');
   };
 
   const handleShowPassword = () => {
     setShowPW(!showPW);
   };
-  const handleIsSame = async () => {
+  const handleIsNameSame = async () => {
     if (nickname === '') {
       setNicknameError('닉네임을 입력해주세요');
+      return;
     } // 닉네임 입력을 안했을 시
-    else {
-      setIsSame(true);
-      await axios
-        .get(`/auth/register/:${nickname}`)
-        .then(() => {
-          setNicknameError('사용 가능한 닉네임입니다');
-        })
-        .catch(() => {
-          setNicknameError('존재하는 닉네임입니다');
-          setIsSame(false);
-        });
-    }
+    setIsNameSame(true);
+    await axios
+      .get(`/auth/register/:${nickname}`)
+      .then(() => {
+        setNicknameError('사용 가능한 닉네임입니다');
+      })
+      .catch(() => {
+        setNicknameError('존재하는 닉네임입니다');
+        setIsNameSame(false);
+      });
   }; // 중복 확인
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (isSame) {
+    const emailRegrex =
+      /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+    const passwordRegrex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{7,}$/;
+
+    if (isNameSame) {
       const data = new FormData(event.currentTarget);
       const joinData = {
         email: data.get('email'),
         password: data.get('password'),
         stack: data.get('stack'),
       };
-      const emailRegrex =
-        /([\w-.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-      const passwordRegrex = /^(?=.*[a-zA-Z])(?=.*[0-9]).{7,}$/;
       if (!emailRegrex.test(joinData.email)) setEmailError('이메일 형식이 올바르지 않습니다');
       else setEmailError('');
       if (!passwordRegrex.test(joinData.password)) setPasswordError('영문 숫자포함 7자 이상의 비밀번호를 설정해주세요');
       else setPasswordError('');
       if (joinData.stack === '') setStackError('스택을 선택해주세요');
       else setStackError('');
-    } else {
-      Swal.fire({
-        width: 400,
-        height: 260,
-        html: '닉네임 중복을 먼저 확인해주세요',
-        showConfirmButton: false,
-        cancelButtonText: '확인',
-        cancelButtonColor: '#CF5E53',
-        showCancelButton: true,
-        timer: 3000,
-      });
+      return;
     }
+    Swal.fire({
+      width: 400,
+      height: 260,
+      html: '닉네임 중복을 먼저 확인해주세요',
+      showConfirmButton: false,
+      cancelButtonText: '확인',
+      cancelButtonColor: '#CF5E53',
+      showCancelButton: true,
+      timer: 3000,
+    });
   };
 
   return (
@@ -163,7 +163,6 @@ function Join() {
                   <Text sx={TextStyle}>Email</Text>
                   <Box sx={inputGroupStyle}>
                     <Input
-                      id="email"
                       name="email"
                       w="450px"
                       placeholder="이메일 입력 ex)abc@gmail.com"
@@ -179,7 +178,6 @@ function Join() {
                   <Box sx={inputGroupStyle}>
                     <InputGroup w="450px">
                       <Input
-                        id="password"
                         name="password"
                         placeholder="영문 숫자 포함 7자 이상"
                         sx={inputStyle}
@@ -206,7 +204,7 @@ function Join() {
                     <Box w="450px" display="flex">
                       <Input
                         value={nickname}
-                        onChange={handlenicknameChange}
+                        onChange={handleNickNameChange}
                         placeholder="닉네임을 입력해주세요"
                         sx={inputStyle}
                         _focusVisible={{ borderColor: 'black' }}
@@ -216,13 +214,13 @@ function Join() {
                         _hover={{ backgroundColor: '#3182CE', opacity: '0.8' }}
                         sx={buttonColor}
                         fontWeight="400"
-                        onClick={handleIsSame}
-                        isDisabled={isSame}
+                        onClick={handleIsNameSame}
+                        isDisabled={isNameSame}
                       >
                         중복
                       </Button>
                     </Box>
-                    <FormHelperText color={isSame ? '#0AA322' : '#ff0000'} fontWeight="500" fontSize="0.813rem">
+                    <FormHelperText color={isNameSame ? '#0AA322' : '#ff0000'} fontWeight="500" fontSize="0.813rem">
                       {nicknameError}
                     </FormHelperText>
                   </Box>
@@ -231,7 +229,6 @@ function Join() {
                   <Text sx={TextStyle}>Stack</Text>
                   <Box sx={inputGroupStyle}>
                     <Select
-                      id="stack"
                       name="stack"
                       placeholder="&nbsp;&nbsp;&nbsp;&nbsp;Stack"
                       variant="unstyled"
